c50580ac26b1e5958a274561e121185594a793bb
Junior <guitar.sound12@gmail.com>
2022-11-26 08:41:43 -0500

    otra prueba

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Primer inento</title>
 </head>
 <body>
     <h1>Hey bro, i'm here</h1>
-    <p>maldicion</p>
+    <p>Ya estamos en la web Bro</p>
     <p>Al parecer ya funciona</p>
 </body>
 <script src="./prueba.js"></script>
